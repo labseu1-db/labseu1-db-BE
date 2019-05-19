d141401377e496a4cdf6a2fad5d7aaa539a9ac8c
sean-attewell <sean.attewell@gmail.com>
2019-05-19 17:43:23 +0100

    one off test payment working

---
+++ stripe-server.js
@@ -0,0 +1,23 @@
+const app = require('express')();
+const stripe = require('stripe')('sk_test_Fp1czcN7sosPfrOFPrZz1Ddu00JL3WqPgd');
+const cors = require('cors');
+
+app.use(require('body-parser').text());
+app.use(cors());
+
+app.post('/charge', async (req, res) => {
+  try {
+    let { status } = await stripe.charges.create({
+      amount: 2000, // in cents
+      currency: 'usd',
+      description: 'An example charge',
+      source: req.body
+    });
+
+    res.json({ status });
+  } catch (err) {
+    res.status(500).end();
+  }
+});
+
+app.listen(9000, () => console.log('Listening on port 9000'));
