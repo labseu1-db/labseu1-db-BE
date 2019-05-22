1c01cf61ae0b9a1c100fcd12c44b15842c2a4646
Thorben Bender <bender.thorben0@gmail.com>
2019-05-22 16:03:52 +0200

    added a console.log for a scuccessfull run

--- functions/index.js
+++ functions/index.js
@@ -48,6 +48,8 @@ async function sendWelcomeEmail(email) {
   await mailTransport.sendMail(mailOptions, (err, res) => {
     if (err) {
       console.log("Error:", err);
+    } else {
+      console.log("Email successfully send to", email);
     }
   });
   return null;
